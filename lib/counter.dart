62a85709c52b415ff215dbe55ead83cdcb14af40
leewrigg <l.wrigglesworth@pitchero.com>
2023-03-03 10:45:34 +0000

    Lesson 1

--- lib/counter.dart
+++ lib/counter.dart
@@ -4,8 +4,16 @@ class Counter {
   int value = 0;
   int valueChangedCount = 0;
 
-  void increment() => value = ++value;
-  void decrement() => value = value--;
+  void increment() {
+    value = ++value;
+    valueChangedCount++;
+  }
+
+  void decrement() {
+    value = value - 1;
+    valueChangedCount++;
+  }
+
   void reset() => value == 0;
 
   void random() => value += Random().nextInt(10) - 5; // +/- 5
--- lib/utils.dart
+++ lib/utils.dart
@@ -1,3 +1,20 @@
 class Utils {
-  static String intWithSuffix(int i) => "${i}th";
+  static String intWithSuffix(int i) {
+    assert(i >= 0);
+    final modHundred = i % 100;
+    final endsInTeen = modHundred > 10 && modHundred < 20;
+    if (endsInTeen) {
+      return "${i}th";
+    }
+    switch (i % 10) {
+      case 1:
+        return "${i}st";
+      case 2:
+        return "${i}nd";
+      case 3:
+        return "${i}rd";
+      default:
+        return "${i}th";
+    }
+  }
 }
